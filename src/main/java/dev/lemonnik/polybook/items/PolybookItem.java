39f7b0d6f3975e4b10eb96497ddb7be9942f0e58
NPC-112 <Dragongamer112@gmail.com>
2025-11-12 18:21:18 +0000

    1.21.10 update by updating gradle settings and using AI to explain errors and stack overflow to paste solutions

--- src/main/java/dev/lemonnik/polybook/items/PolybookItem.java
+++ src/main/java/dev/lemonnik/polybook/items/PolybookItem.java
@@ -1,12 +1,14 @@
 package dev.lemonnik.polybook.items;
 
 import dev.lemonnik.polybook.Polybook;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import eu.pb4.polymer.core.api.item.PolymerItem;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.util.Identifier;
@@ -21,9 +23,12 @@ public class PolybookItem extends Item implements PolymerItem {
 
     @Override
     public ActionResult use(World world, PlayerEntity user, Hand hand) {
-        if (!world.isClient) {
-            if (user.isPlayer()) {
-                user.getServer().getCommandManager().executeWithPrefix(user.getCommandSource((ServerWorld) world), "/polydex");
+        if (!world.isClient() && world instanceof ServerWorld serverWorld && user instanceof ServerPlayerEntity serverPlayer) {
+            try {
+                // get the server from the ServerWorld and run the command (no leading '/')
+                serverWorld.getServer().getCommandManager().getDispatcher().execute("polydex", serverPlayer.getCommandSource());
+            } catch (CommandSyntaxException e) {
+                e.printStackTrace(); // or log to your mod logger
             }
         }
         return super.use(world, user, hand);
